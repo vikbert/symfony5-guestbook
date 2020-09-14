71badcc58b0f28d6ca69d7b935f59e47ca5265bd
Xun Zhou <segentor@gmail.com>
2020-09-14 16:31:07 +0200

    add maker bundle and extra-bundle for annotations

--- public/index.php
+++ public/index.php
@@ -19,10 +19,8 @@ if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
-
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
-dump($request);
 $response->send();
 $kernel->terminate($request, $response);
