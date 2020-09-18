05443d5afda967188a1ef7791c8dc77862be94f1
Xun Zhou <segentor@gmail.com>
2020-09-18 10:06:13 +0200

    use workflow to handle the transition of the state of comment

--- src/Message/CommentMessageHandler.php
+++ src/Message/CommentMessageHandler.php
@@ -4,16 +4,25 @@ namespace App\Message;
 
 use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
 
 final class CommentMessageHandler implements MessageHandlerInterface
 {
     private $entityManager;
     private $commentRepository;
-    public function __construct(EntityManagerInterface $entityManager, CommentRepository $commentRepository)
+    private $bus;
+    private $workflow;
+    private $logger;
+    public function __construct(EntityManagerInterface $entityManager, CommentRepository $commentRepository, MessageBusInterface $bus, WorkflowInterface $commentStateMachine, LoggerInterface $logger)
     {
         $this->entityManager = $entityManager;
         $this->commentRepository = $commentRepository;
+        $this->bus = $bus;
+        $this->workflow = $commentStateMachine;
+        $this->logger = $logger;
     }
 
     public function __invoke (CommentMessage $message): void
@@ -23,7 +32,20 @@ final class CommentMessageHandler implements MessageHandlerInterface
             return;
         }
 
-        $comment->setState('published');
+        if ($this->workflow->can($comment, 'accept')) {
+            $this->workflow->apply($comment, 'accept');
+            $this->entityManager->flush();
+            $this->bus->dispatch($message);
+            
+            $this->logger->debug(sprintf('Comment state: [%s]', $comment->getState()));
+        } elseif ($this->workflow->can($comment, 'publish_ham')) {
+            $this->workflow->apply($comment, 'publish_ham');
+            $this->entityManager->flush();
+            
+            $this->logger->debug(sprintf('Comment state: [%s]', $comment->getState()));
+        } else {
+            $this->logger->debug(sprintf('Dropping comment message: %s', $comment->getId()));
+        }
 
         $this->entityManager->flush();
     }
