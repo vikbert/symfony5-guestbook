9b0050d2be5a73594948e9741073d3d789d59030
Xun Zhou <segentor@gmail.com>
2020-09-21 10:09:50 +0200

    api declaration

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -2,16 +2,29 @@
 
 namespace App\Entity;
 
+use DateTime;
 use DateTimeInterface;
 use App\Entity\Conference;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\CommentRepository;
-use DateTime;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 
 /**
  * @ORM\Entity(repositoryClass=CommentRepository::class)
  * @ORM\HasLifecycleCallbacks()
+ * 
+  * @ApiResource(
+ *     collectionOperations={"get"={"normalization_context"={"groups"="comment:list"}}},
+ *     itemOperations={"get"={"normalization_context"={"groups"="comment:item"}}},
+ *     order={"createdAt"="DESC"},
+ *     paginationEnabled=false
+ * )
+ * 
+ * @ApiFilter(SearchFilter::class, properties={"conference": "exact"})
  */
 class Comment
 {
@@ -19,45 +32,61 @@ class Comment
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * 
+     * @Groups({"comment:list", "comment:item"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank
+     * 
+     * @Groups({"comment:list", "comment:item"})
      */
     private $author;
 
     /**
      * @ORM\Column(type="text")
      * @Assert\NotBlank
+     * 
+     * @Groups({"comment:list", "comment:item"})
      */
     private $text;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank 
+     * 
+     * @Groups({"comment:list", "comment:item"})
      */
     private $email;
 
     /**
      * @ORM\Column(type="datetime")
+     * 
+     * @Groups({"comment:list", "comment:item"})
      */
     private $createdAt;
 
     /**
      * @ORM\ManyToOne(targetEntity=Conference::class, inversedBy="comments")
      * @ORM\JoinColumn(nullable=false)
+     * 
+     * @Groups({"comment:list", "comment:item"})
      */
     private $conference;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * 
+     * @Groups({"comment:list", "comment:item"})
      */
     private $photoFilename;
 
     /**
      * @ORM\Column(type="string", length=32, nullable=false, options={"default": "submitted"})
+     * 
+     * @Groups({"comment:list", "comment:item"})
      */
     private $state = "submitted";
 
